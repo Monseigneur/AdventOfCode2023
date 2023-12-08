88df905c32eeafd439383f8ac145cdd41037f598
Milan Justel <milancjustel@gmail.com>
2023-12-07 20:38:51 -0800

    Finished day 4

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,4 @@
-use std::{fs, collections::HashSet};
+use std::{fs, collections::HashSet, collections::HashMap};
 
 pub fn run() {
     let contents = fs::read_to_string("test_files/day4/input.txt").unwrap();
@@ -14,18 +14,11 @@ fn part_1(data: &str) -> u32 {
 
     let mut points: u32 = 0;
 
-    for line in data.lines() {
-        let card_info: Vec<&str> = line.split("|").collect();
+    let matching_numbers = find_matching_numbers(data);
 
-        let winning_numbers = card_info[0].split(":").collect::<Vec<&str>>()[1];
-
-        let winning_numbers = parse_numbers(winning_numbers);
-        let card_numbers = parse_numbers(card_info[1]);
-
-        let intersection_size = winning_numbers.intersection(&card_numbers).collect::<Vec<&u32>>().len() as u32;
-
-        if intersection_size != 0 {
-            points += 2_u32.pow(intersection_size - 1) as u32;
+    for (_, num_matching) in matching_numbers {
+        if num_matching != 0 {
+            points += 2_u32.pow(num_matching - 1) as u32;
         }
     }
 
@@ -46,6 +39,94 @@ fn parse_numbers(data: &str) -> HashSet<u32> {
     results
 }
 
+fn find_matching_numbers(data: &str) -> HashMap<u32, u32> {
+    let mut matching_numbers: HashMap<u32, u32> = HashMap::new();
+
+    for line in data.lines() {
+        let card_info: Vec<&str> = line.split("|").collect();
+
+        let first_piece: Vec<&str> = card_info[0].split(":").collect();
+
+        // println!("first_piece {:?}", first_piece);
+
+        let card_title: Vec<&str> = first_piece[0].split(" ").filter(|s| !s.is_empty()).collect();
+        // println!("  card_title {:?}", card_title);
+        let card_number: u32 = card_title[1].trim().parse().unwrap();
+
+        let winning_numbers = parse_numbers(first_piece[1]);
+        let card_numbers = parse_numbers(card_info[1]);
+
+        let intersection_size = winning_numbers.intersection(&card_numbers).collect::<Vec<&u32>>().len() as u32;
+
+        matching_numbers.insert(card_number, intersection_size);
+    }
+
+    matching_numbers
+}
+
 fn part_2(data: &str) -> u32 {
-    1
+    let matching_numbers = find_matching_numbers(data);
+
+    // 1->4, 2->2, 3->2, 4->1, 5->0, 6->0
+    // println!("matching_numbers {:?}", matching_numbers);
+
+    println!("num cards {}", matching_numbers.len());
+
+    let mut card_counts = CardCounts::new(matching_numbers.len());
+
+    for i in (1..=matching_numbers.len()).rev() {
+        let val = matching_numbers.get(&(i as u32));
+
+        assert!(val.is_some());
+
+        let num_matches = val.unwrap();
+
+        card_counts.calc_cards(i, *num_matches);
+    }
+
+    // println!("card_counts {:?}", card_counts);
+
+    let mut total = 0;
+    for i in 1..=matching_numbers.len() {
+        total += card_counts.get_count(i);
+    }
+
+    // 9903606 is wrong for part 2
+    // 9924412
+    total
+}
+
+struct CardCounts {
+    counts: Vec<u32>
+}
+
+impl CardCounts {
+    fn new(num_cards: usize) -> Self {
+        Self {
+            counts: vec![0; num_cards]
+        }
+    }
+
+    fn get_count(&self, card_id: usize) -> u32 {
+        assert!(card_id <= self.counts.len());
+
+        self.counts[card_id - 1]
+    }
+
+    fn calc_cards(&mut self, card_id: usize, num_matches: u32) {
+        assert!(card_id <= self.counts.len());
+
+        // If num_matches is 0, only win this card.
+        // Else, win the next num_matches cards.
+
+        let mut count = 1;
+
+        if num_matches != 0 {
+            for i in 0..num_matches {
+                count += self.counts[card_id + i as usize];
+            }
+        }
+
+        self.counts[card_id - 1] = count;
+    }
 }
