41fdb53d6fe6f183c13b0a695f8ee9b1274c3a0d
Milan Justel <milancjustel@gmail.com>
2023-12-08 19:11:18 -0800

    Finished day 5 part 1

---
+++ src/day5.rs
@@ -0,0 +1,162 @@
+use std::fs;
+
+pub fn run() {
+    let contents = fs::read_to_string("test_files/day5/input.txt").unwrap();
+
+    let part_1_result = part_1(&contents);
+    let part_2_result = part_2(&contents);
+
+    println!("[Day 5]: part 1: {part_1_result}, part 2: {part_2_result}");
+}
+
+// Example data:
+// seeds: 79 14 55 13
+//
+// seed-to-soil map:
+// 50 98 2
+// 52 50 48
+//
+// The map lines are <DEST_START> <SOURCE_START> <LEN>, and it's linear in that range.
+// ex: 50 98 2
+//     DEST_START=50, SOURCE_START=98, LEN=2 ->
+//      98 <= seed <= 99 -> seed - 98 + 50
+//      seed 98 -> soil 50
+//      seed 99 -> soil 51
+//     52 50 48
+//     DEST_START=52, SOURCE_START=50, LEN=48 ->
+//      50 <= seed <= 97 -> seed - 50 + 52
+//      seed 50 -> soil 52
+//      ...
+//      seed 97 -> soil
+//     If doesn't match a range, then DEST = SOURCE
+//      SOURCE_START <= seed <= SOURCE_START + LEN - 1 -> seed - SOURCE_START + DEST_START
+
+fn part_1(data: &str) -> isize {
+    let (seeds, maps) = parse_data(data);
+
+    let min = seeds
+        .iter()
+        .map(|seed| apply_maps(*seed, &maps))
+        .min()
+        .unwrap();
+
+    min
+}
+
+fn parse_data(data: &str) -> (Vec<isize>, Vec<Map>) {
+    let mut line_iter = data.lines();
+
+    let seeds_line = line_iter.next().unwrap().split(":").last().unwrap();
+    line_iter.next();
+
+    let seeds: Vec<isize> = seeds_line
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<isize>().unwrap())
+        .collect();
+
+    let mut maps = vec![];
+
+    let mut map_started = false;
+    let mut map_lines = vec![];
+    while let Some(line) = line_iter.next() {
+        if line.contains("map") {
+            map_started = true;
+
+            continue;
+        }
+
+        if line.is_empty() {
+            maps.push(Map::new(&map_lines));
+            map_lines.clear();
+
+            map_started = false;
+
+            continue;
+        }
+
+        if !map_started {
+            continue;
+        }
+
+        map_lines.push(line);
+    }
+
+    if !map_lines.is_empty() {
+        maps.push(Map::new(&map_lines));
+    }
+
+    (seeds, maps)
+}
+
+#[derive(Debug)]
+struct Range {
+    start: isize,
+    len: isize,
+    delta: isize,
+}
+
+impl Range {
+    fn new(dest: isize, start: isize, len: isize) -> Self {
+        Self {
+            start,
+            len,
+            delta: dest - start,
+        }
+    }
+
+    fn contains(&self, value: isize) -> bool {
+        value >= self.start && value < self.start + self.len
+    }
+
+    fn apply(&self, value: isize) -> isize {
+        value + self.delta
+    }
+}
+
+#[derive(Debug)]
+struct Map {
+    ranges: Vec<Range>,
+}
+
+impl Map {
+    fn new(lines: &Vec<&str>) -> Self {
+        let mut ranges = Vec::new();
+
+        for line in lines {
+            let pieces = line.split_ascii_whitespace().collect::<Vec<&str>>();
+
+            assert!(pieces.len() == 3);
+
+            let pieces: Vec<isize> = pieces.iter().map(|s| s.parse::<isize>().unwrap()).collect();
+
+            ranges.push(Range::new(pieces[0], pieces[1], pieces[2]));
+        }
+
+        Self { ranges }
+    }
+
+    fn map(&self, input: isize) -> isize {
+        for range in &self.ranges {
+            if range.contains(input) {
+                let result = range.apply(input);
+                return result;
+            }
+        }
+
+        input
+    }
+}
+
+fn apply_maps(seed: isize, maps: &Vec<Map>) -> isize {
+    let mut result = seed;
+
+    for map in maps {
+        result = map.map(result);
+    }
+
+    result
+}
+
+fn part_2(_data: &str) -> u32 {
+    52
+}
