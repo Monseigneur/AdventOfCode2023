d5d8639e600a493acd30dbf51e5f534c97b5d929
Milan Justel <milancjustel@gmail.com>
2023-12-10 20:42:14 -0800

    Completed day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -50,6 +50,29 @@ fn calc_line_delta(line: &LineData) -> LineData {
         .collect::<LineData>()
 }
 
-fn part_2(data: &str) -> usize {
-    0
+// This is the same, but in reverse, extrapolating the value before the first value.
+fn part_2(data: &str) -> isize {
+    data.lines().map(|line| process_line_v2(line)).sum()
+}
+
+fn process_line_v2(line: &str) -> isize {
+    let mut line_numbers = convert_line(line);
+
+    assert!(!line_numbers.is_empty());
+
+    // Need to walk backwards in the list of first elements, so have to keep
+    // track of them all at first.
+    let mut first_vals = vec![];
+
+    first_vals.push(*line_numbers.iter().next().unwrap());
+
+    while !is_all_zero(&line_numbers) {
+        let new_line_numbers = calc_line_delta(&line_numbers);
+
+        first_vals.push(*new_line_numbers.iter().next().unwrap_or(&0));
+
+        line_numbers = new_line_numbers;
+    }
+
+    first_vals.iter().rfold(0, |acc, val| val - acc)
 }
