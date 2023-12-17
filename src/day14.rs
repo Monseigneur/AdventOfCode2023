d5b5f8d2e8e0e3c9fc1fd310ec1956386ee80974
Milan Justel <milancjustel@gmail.com>
2023-12-16 21:22:14 -0800

    Completed day 14 part 2, solution isn't great

--- src/day14.rs
+++ src/day14.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 use utilities;
@@ -57,6 +58,182 @@ fn process_column(data: &Data, col: usize) -> usize {
     score
 }
 
+// Instead of just tilting to the north, tilt in a cycle of north, west, south, east. After 1000000000 cycles
+// calculate the weight on the north supports.
 fn part_2(data: &str) -> usize {
-    0
+    const ITERATIONS: usize = 1_000_000_000;
+
+    let mut data = data
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect::<Data>();
+
+    // It's likely that the rock arrangements stabilize after a while. Track the arrangements to find when
+    // a pattern is repeated.
+    let mut patterns: HashMap<String, usize> = HashMap::new();
+
+    patterns.insert(build_string(&data), 0);
+
+    let mut result_cycle_count = None;
+
+    for i in 0..ITERATIONS {
+        let cycle_count = i + 1;
+
+        apply_cycle(&mut data);
+
+        let new = build_string(&data);
+
+        if patterns.contains_key(&new) {
+            // The pattern is cycling, determine the index that would extrapolate to 1,000,000,000.
+            let original_cycle_count = patterns.get(&new).unwrap();
+
+            let delta = i - original_cycle_count;
+            let offset = (ITERATIONS - original_cycle_count) % delta;
+
+            result_cycle_count = Some(original_cycle_count + offset);
+
+            break;
+        }
+
+        patterns.insert(new, cycle_count);
+    }
+
+    // From the resulting extrapolated arrangement, calculate the weight score for that pattern.
+    assert!(result_cycle_count.is_some());
+
+    for (str_data, index) in patterns {
+        if index == result_cycle_count.unwrap() {
+            return calculate_string_weight(&str_data, data.len(), data[0].len());
+        }
+    }
+
+    panic!("No score found!");
+}
+
+fn build_string(data: &Data) -> String {
+    data.iter()
+        .map(|row| row.iter().collect::<String>())
+        .fold(String::new(), |s, row| s + &row)
+}
+
+fn apply_cycle(data: &mut Data) {
+    for col in 0..data[0].len() {
+        tilt_column_north(data, col);
+    }
+
+    for row in 0..data.len() {
+        tilt_row_west(data, row);
+    }
+
+    for col in 0..data[0].len() {
+        tilt_column_south(data, col);
+    }
+
+    for row in 0..data.len() {
+        tilt_row_east(data, row);
+    }
+}
+
+fn tilt_column_north(data: &mut Data, col: usize) {
+    let mut open_index = 0;
+
+    for row in 0..data.len() {
+        let position = row;
+
+        match data[row][col] {
+            'O' => {
+                data[position][col] = '.';
+                data[open_index][col] = 'O';
+
+                open_index += 1;
+            }
+            '#' => open_index = position + 1,
+            _ => (),
+        };
+    }
+}
+
+fn tilt_row_west(data: &mut Data, row: usize) {
+    let mut open_index = 0;
+
+    for col in 0..data[0].len() {
+        match data[row][col] {
+            'O' => {
+                data[row][col] = '.';
+                data[row][open_index] = 'O';
+
+                open_index += 1;
+            }
+            '#' => open_index = col + 1,
+            _ => (),
+        };
+    }
+}
+
+fn tilt_column_south(data: &mut Data, col: usize) {
+    let length = data.len();
+    let mut open_index = length - 1;
+
+    for row in 0..data.len() {
+        let position = length - row - 1;
+
+        match data[position][col] {
+            'O' => {
+                data[position][col] = '.';
+                data[open_index][col] = 'O';
+
+                if open_index != 0 {
+                    open_index -= 1;
+                }
+            }
+            '#' => {
+                if position != 0 {
+                    open_index = position - 1;
+                }
+            }
+            _ => (),
+        };
+    }
+}
+
+fn tilt_row_east(data: &mut Data, row: usize) {
+    let length = data[0].len();
+    let mut open_index = length - 1;
+
+    for col in 0..data[0].len() {
+        let position = length - col - 1;
+
+        match data[row][position] {
+            'O' => {
+                data[row][position] = '.';
+                data[row][open_index] = 'O';
+
+                if open_index != 0 {
+                    open_index -= 1;
+                }
+            }
+            '#' => {
+                if position != 0 {
+                    open_index = position - 1;
+                }
+            }
+            _ => (),
+        };
+    }
+}
+
+fn calculate_string_weight(data: &str, num_rows: usize, num_cols: usize) -> usize {
+    let mut score = 0;
+
+    assert!(num_rows * num_cols == data.len());
+
+    for row in 0..num_rows {
+        let row_data = &data[(row * num_cols)..((row + 1) * num_cols)];
+
+        let round_rock_count = row_data.chars().filter(|&tile| tile == 'O').count();
+
+        score += round_rock_count * (num_rows - row);
+    }
+
+    score
 }
