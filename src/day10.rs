f09858dd8734f73c381d4933f99d85a62819318b
Milan Justel <milancjustel@gmail.com>
2023-12-17 21:23:10 -0800

    Completed day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -48,7 +48,7 @@ fn part_1(data: &str) -> usize {
 
     while direction != Direction::Start {
         count += 1;
-        direction = make_move(&data, &current, direction, &tile_lookup);
+        direction = make_move(&data, &current, &direction, &tile_lookup);
 
         current.move_direction(&direction);
     }
@@ -56,7 +56,7 @@ fn part_1(data: &str) -> usize {
     count / 2
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 enum Direction {
     Start,
     North,
@@ -178,7 +178,7 @@ fn find_start(data: &Vec<Vec<char>>, animal: &Point, lookup: &TileLookup) -> (Po
 fn make_move(
     data: &Vec<Vec<char>>,
     current: &Point,
-    prev_direction: Direction,
+    prev_direction: &Direction,
     lookup: &TileLookup,
 ) -> Direction {
     let current_tile = data[current.row][current.col];
@@ -190,6 +190,278 @@ fn make_move(
     }
 }
 
+// Figure out how many tiles are within the bounds of the loop.
 fn part_2(data: &str) -> usize {
-    0
+    let data = data
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect::<Vec<Vec<char>>>();
+
+    // First find the loop and remove other tiles.
+    let mut tile_data: Vec<Vec<char>> = vec![vec!['.'; data[0].len()]; data.len()];
+
+    let animal = find_animal(&data);
+
+    tile_data[animal.row][animal.col] = data[animal.row][animal.col];
+
+    let tile_lookup = TileLookup::new();
+
+    // Search for the start of the loop
+    let (mut current, mut direction) = find_start(&data, &animal, &tile_lookup);
+
+    while direction != Direction::Start {
+        tile_data[current.row][current.col] = data[current.row][current.col];
+
+        direction = make_move(&data, &current, &direction, &tile_lookup);
+
+        current.move_direction(&direction);
+    }
+
+    // New idea, with the path, start from the edge and find the wall. At that point, start following the path in
+    // the up/right direction. Now, everything on the left is outside, and the right is inside.
+
+    replace_animal(&mut tile_data, &animal, &tile_lookup);
+
+    // First find first wall
+    let mut starting_wall = None;
+
+    'outer: for col in 0..tile_data[0].len() {
+        for row in 0..tile_data.len() {
+            match tile_data[row][col] {
+                '.' => {
+                    tile_data[row][col] = 'O';
+                    continue;
+                }
+                _ => {
+                    starting_wall = Some(Point::new(row, col));
+
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    assert!(starting_wall.is_some());
+
+    let starting_wall = starting_wall.unwrap();
+
+    let (mut current, mut direction) = find_start(&tile_data, &starting_wall, &tile_lookup);
+
+    let num_rows = tile_data.len();
+    let num_cols = tile_data[0].len();
+
+    while tile_data[current.row][current.col] != 'W' {
+        let prev_direction = direction;
+        direction = make_move(&tile_data, &current, &prev_direction, &tile_lookup);
+
+        // Set current tile to W for wall, and left to O and right to I.
+        tile_data[current.row][current.col] = 'W';
+
+        if let Some(outside_point) =
+            get_adjacent_point(&current, &prev_direction, num_rows, num_cols, true)
+        {
+            if tile_data[outside_point.row][outside_point.col] == '.' {
+                tile_data[outside_point.row][outside_point.col] = 'O';
+            }
+        }
+
+        if let Some(inside_point) =
+            get_adjacent_point(&current, &prev_direction, num_rows, num_cols, false)
+        {
+            if tile_data[inside_point.row][inside_point.col] == '.' {
+                tile_data[inside_point.row][inside_point.col] = 'I';
+            }
+        }
+
+        if let Some(outside_point) =
+            get_adjacent_point(&current, &direction, num_rows, num_cols, true)
+        {
+            if tile_data[outside_point.row][outside_point.col] == '.' {
+                tile_data[outside_point.row][outside_point.col] = 'O';
+            }
+        }
+
+        if let Some(inside_point) =
+            get_adjacent_point(&current, &direction, num_rows, num_cols, false)
+        {
+            if tile_data[inside_point.row][inside_point.col] == '.' {
+                tile_data[inside_point.row][inside_point.col] = 'I';
+            }
+        }
+
+        current.move_direction(&direction);
+    }
+
+    flood_fill(&mut tile_data);
+
+    tile_data.iter().fold(0, |acc, row_data| {
+        acc + row_data.iter().filter(|&&tile| tile == 'I').count()
+    })
+}
+
+fn replace_animal(tile_data: &mut Vec<Vec<char>>, animal: &Point, lookup: &TileLookup) {
+    let mut north_wall = false;
+
+    if animal.row > 0 {
+        if lookup.allow_entry(tile_data[animal.row - 1][animal.col], &Direction::North) {
+            north_wall = true;
+        }
+    }
+
+    let mut east_wall = false;
+
+    if animal.col < tile_data[0].len() - 1 {
+        if lookup.allow_entry(tile_data[animal.row][animal.col + 1], &Direction::East) {
+            east_wall = true;
+        }
+    }
+
+    let mut south_wall = false;
+
+    if animal.row < tile_data.len() - 1 {
+        if lookup.allow_entry(tile_data[animal.row + 1][animal.col], &Direction::South) {
+            south_wall = true;
+        }
+    }
+
+    let mut west_wall = false;
+
+    if animal.col > 0 {
+        if lookup.allow_entry(tile_data[animal.row][animal.col - 1], &Direction::West) {
+            west_wall = true;
+        }
+    }
+
+    // Wall types:
+    //  | NS
+    //  - EW
+    //  L NE
+    //  J NW
+    //  7 SW
+    //  F SE
+
+    let new_animal = if north_wall && south_wall {
+        '|'
+    } else if north_wall && east_wall {
+        'L'
+    } else if north_wall && west_wall {
+        'J'
+    } else if east_wall && west_wall {
+        '-'
+    } else if east_wall && south_wall {
+        'F'
+    } else {
+        '7'
+    };
+
+    tile_data[animal.row][animal.col] = new_animal;
+}
+
+fn get_adjacent_point(
+    current: &Point,
+    direction: &Direction,
+    num_rows: usize,
+    num_cols: usize,
+    left: bool,
+) -> Option<Point> {
+    if left {
+        match direction {
+            Direction::North => {
+                if current.col > 0 {
+                    return Some(Point::new(current.row, current.col - 1));
+                }
+            }
+            Direction::East => {
+                if current.row > 0 {
+                    return Some(Point::new(current.row - 1, current.col));
+                }
+            }
+            Direction::South => {
+                if current.col < num_cols - 1 {
+                    return Some(Point::new(current.row, current.col + 1));
+                }
+            }
+            Direction::West => {
+                if current.row < num_rows - 1 {
+                    return Some(Point::new(current.row + 1, current.col));
+                }
+            }
+            _ => return None,
+        }
+    } else {
+        match direction {
+            Direction::North => {
+                if current.col < num_cols - 1 {
+                    return Some(Point::new(current.row, current.col + 1));
+                }
+            }
+            Direction::East => {
+                if current.row < num_rows - 1 {
+                    return Some(Point::new(current.row + 1, current.col));
+                }
+            }
+            Direction::South => {
+                if current.col > 0 {
+                    return Some(Point::new(current.row, current.col - 1));
+                }
+            }
+            Direction::West => {
+                if current.row > 0 {
+                    return Some(Point::new(current.row - 1, current.col));
+                }
+            }
+            _ => return None,
+        }
+    }
+
+    None
+}
+
+fn flood_fill(tile_data: &mut Vec<Vec<char>>) {
+    let num_rows = tile_data.len();
+    let num_cols = tile_data[0].len();
+
+    loop {
+        let mut open_ground_count = 0;
+
+        for row in 0..num_rows {
+            for col in 0..num_cols {
+                fill_around(tile_data, row, col);
+            }
+
+            for col in 0..num_cols {
+                if tile_data[row][col] == '.' {
+                    open_ground_count += 1;
+                }
+            }
+        }
+
+        if open_ground_count == 0 {
+            break;
+        }
+    }
+}
+
+fn fill_around(tile_data: &mut Vec<Vec<char>>, row: usize, col: usize) {
+    let current_tile = tile_data[row][col];
+
+    if current_tile == 'W' {
+        return;
+    }
+
+    if row > 0 && tile_data[row - 1][col] == '.' {
+        tile_data[row - 1][col] = current_tile;
+    }
+
+    if col < tile_data[0].len() - 1 && tile_data[row][col + 1] == '.' {
+        tile_data[row][col + 1] = current_tile;
+    }
+
+    if row < tile_data.len() - 1 && tile_data[row + 1][col] == '.' {
+        tile_data[row + 1][col] = current_tile;
+    }
+
+    if col > 0 && tile_data[row][col - 1] == '.' {
+        tile_data[row][col - 1] = current_tile;
+    }
 }
